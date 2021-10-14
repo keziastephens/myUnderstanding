ff99c37e13a2a72a4ed5b4ec1c063ffc1649a02d
keziastephens <87955878+keziastephens@users.noreply.github.com>
2021-10-14 20:28:31 +1300

    Add files via upload

---
+++ my understanding/script.js
@@ -0,0 +1,68 @@
+// variables
+
+// const (consistent) variables carry have a constant variable
+const one = 1;
+
+// let variables are similar to const variables but are only
+// relative to themselves
+let one = 1;
+
+// var?? just regular idk we havent rly used em
+// assume its just like let, but apparently not as good
+// don't really understand why lol
+
+
+
+
+// data types
+
+// numbers - numerical data 
+let one = 1;
+
+// strings allow us to create sentences
+// difference is that it uses "" 
+let one = "this is the number one";
+
+// booleans
+// true or false
+// don't really understand these
+
+// null or NaN - meaning 0
+let zero = null;
+
+// undefined - has not been given a variable
+let one;
+
+
+
+
+// conditions and comparisons - off the top of my head nothing
+
+// google helped me out for a real one tho
+
+// == - equal - returns true if the operands are equal
+// != - not equal - returns true if the operands are not equal
+// === - strict equal - returns true if the operands are equal and of the same type
+// !== - strict not equal - returns true if the operands are of the same type but not equal, or are of a different type
+// > - greater than - returns true if the left operand is greater than the right
+// >= - greater/equal than - returns true if the left operand is greater than or equal to the right operand
+// < - less than - returns true if the left operand is less than the right operand
+// <= - less/equal than - true if the left operand is less than or equal to the right operand
+
+
+
+
+// loops
+// off the back of my head i completely forgot about these
+
+// functions
+// functions help your code do stuff 
+// i completely forgot the sequence but you put a variable in and can run it from there
+// also you pass data from variable to variable with functions
+// i also forgot about parameters
+// i cannot learn very well when trying to cram everything in lol
+
+// arrays - help you get a bunch of things together compiled into one 
+let alcohol = ['jack daniels', 'vodka', 'whiskey', 'black heart']
+
+// from here you can create objects, give them ids. the thing we used to make cards :)
